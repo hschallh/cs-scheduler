3d997922abb0f16a61b65868844c759940673ca6
hschallh <hschallh@gmail.com>
2017-08-16 14:51:03 -0400

    made the list of courses and object for easier access.  handled nested prereqs.  only works when accessing in order. needs testing and revisiting. references #1.

--- script.js
+++ script.js
@@ -1,98 +1,81 @@
-var courses = [{
-        id: "CS161",
+var courses = {
+    CS161: {
         name: "Intro 1"
     },
-    {
-        id: "CS225",
+    CS225: {
         name: "Discrete Structures",
     },
-    {
-        id: "CS162",
+    CS162: {
         name: "Intro 2",
         prereqs: ["CS161"]
     },
-    {
-        id: "CS352",
+    CS352: {
         name: "Introduction to Usability Engineering",
         elective: true,
         prereqs: ["CS161"]
     },
-    {
-        id: "CS271",
+    CS271: {
         name: "Computer Architecture & Assembly Language",
         prereqs: ["CS161"]
     },
-    {
-        id: "CS261",
+    CS261: {
         name: "Data Structures",
         prereqs: ["CS162", "CS225"]
     },
-    {
-        id: "CS290",
+    CS290: {
         name: "Web Development",
         prereqs: ["CS162"]
     },
-    {
-        id: "CS340",
+    CS340: {
         name: "Introduction to Databases",
         prereqs: ["CS290"]
     },
-    {
-        id: "CS325",
+    CS325: {
         name: "Analysis of Algorithms",
         prereqs: ["CS261", "CS225"]
     },
-    {
-        id: "CS344",
+    CS344: {
         name: "Operating Systems",
         prereqs: ["CS261", "CS271"]
     },
-    {
-        id: "CS361",
+    CS361: {
         name: "Software Engineering I",
         prereqs: ["CS261"]
     },
-    {
-        id: "CS362",
+    CS362: {
         name: "Software Engineering II",
         prereqs: ["CS261"]
     },
-    {
-        id: "CS372",
+    CS372: {
         name: "Intro to Computer Networks",
         prereqs: ["CS271", "CS261"]
     },
-    {
-        id: "CS475",
+    CS475: {
         name: "Parallel Programming",
         elective: true,
         prereqs: ["CS261"]
     },
-    {
-        id: "CS373",
+    CS373: {
         name: "Defense Against the Dark Arts",
         elective: true,
         prereqs: ["CS340", "CS372", "CS344"]
     },
-    {
-        id: "CS464",
+    CS464: {
         name: "Open Source Software Development",
         elective: true,
         prereqs: ["CS361"]
     },
-    {
-        id: "CS496",
+    CS496: {
         name: "Mobile and Cloud Software Development",
         elective: true,
         prereqs: ["CS344"]
     },
-    {
-        id: "CS467",
+    CS467: {
         name: "Software Projects",
         elective: true,
         prereqs: ["CS361"]
     }
-];
+};
 
 
 var today, // Today's date
@@ -117,6 +100,8 @@ var dragAndDrop = dragula({
 // Any time the schedule is changed
 dragAndDrop.on("drop", function(el, target, source, sibling) {
 
+    // If the course was dropped into a button, "click" the button and move the
+    // course to the newly created quarter
     if (target.id == "prevButton") {
         var newQuarter = addPrev();
         $("#" + newQuarter).append(el);
@@ -127,38 +112,35 @@ dragAndDrop.on("drop", function(el, target, source, sibling) {
 
     // add prereq class to courses with unsatisfied prereqs by checking if the
     // course prereqs are still in the courses container.
-    courses.forEach(function(course) {
-        if (course.prereqs) {
+    for (var id in courses) {
+        if (courses[id].prereqs) {
             var satisfied = true;
-            course.prereqs.forEach(function(prereq) {
+            courses[id].prereqs.forEach(function(prereq) {
                 if ($("#" + prereq).parent()[0] == $("#courses")[0]) {
-                    $("#" + course.id).addClass("prereq");
+                    if ($("#" + id).parent().parent()[0] == $("#quarters")[0]) {
+                        $(sibling).before($("#" + id));
+                    }
+                    $("#" + id).addClass("prereq");
                     satisfied = false;
                 }
             });
-            if (satisfied) $("#" + course.id).removeClass("prereq");
+            if (satisfied) $("#" + id).removeClass("prereq");
         }
-    });
-
-    // Now that all of the invalid classes are tagged, move any on the schedule
-    // back to the courses pane
-    $("#quarters *.prereq").each(function() {
-        $(sibling).before(this);
-    });
+    }
 });
 
 // jquery after page loads
 $(function() {
 
     // Build html elements for each of the courses
-    courses.forEach(function(course) {
-        var html = "<div id='" + course.id + "'";
-        html += "><strong>" + course.id + "</strong> - " + course.name + "</div>";
+    for (var id in courses) {
+        var html = "<div id='" + id + "'";
+        html += "><strong>" + id + "</strong> - " + courses[id].name + "</div>";
         $("#courses").append(html);
 
-        if (course.elective) $("#" + course.id).addClass("ele");
-        if (course.prereqs) $("#" + course.id).addClass("prereq");
-    });
+        if (courses[id].elective) $("#" + id).addClass("ele");
+        if (courses[id].prereqs) $("#" + id).addClass("prereq");
+    }
 
     // Label the initial quarter container with the current quarter
     $("#Q0").append(getQuarterName(today));
